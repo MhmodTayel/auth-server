11a4ef8c0c82c29a1ee728019957fd66894c5359
Mahmoud Mohamed Tayel <mhmodtayel0050@gmail.com>
2025-12-01 12:03:06 +0200

    feat: add configuration module with environment validation

---
+++ src/config/config.ts
@@ -0,0 +1,26 @@
+import { registerAs } from '@nestjs/config';
+
+
+interface DatabaseConfig {
+  url: string;
+}
+
+export interface AppConfig {
+  port: number | string;
+}
+
+export interface Config {
+  database: DatabaseConfig;
+  app: AppConfig;
+}
+
+export default registerAs('', (): Config => {
+  return {
+    database: {
+      url: process.env.MONGO_URI || 'mongodb://localhost:27017/authDB',
+    },
+    app: {
+      port: process.env.PORT || 3000,
+    },
+  };
+});
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Config } from './config/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService<Config, true>)
+  const appConfig = configService.get('app', { infer: true })
+  await app.listen(appConfig.port ?? 3000);
 }
 bootstrap();
